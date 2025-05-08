37e65dfb46b8433b5f56c8b8ab57d4935dada45b
Paul Harawa <paulharawa@yahoo.com>
2025-05-08 12:05:02 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import heroImage from "../assets/hero.jpg";
 import heroBg from "../assets/bg.jpg";
